ac711d318e1790bba762c501cce2797335b129e1
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 18:50:09 +0300

    Scripted auto-commit on change (2019-11-15 18:50:09) by gitwatch.sh

--- src/main/java/lab4/StorageActor.java
+++ src/main/java/lab4/StorageActor.java
@@ -1,4 +1,14 @@
 package lab4;
 
-public class StorageActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.*;
+
+public class StorageActor extends AbstractActor {
+    /*б. В приложении будем использовать следующие акторы :
+    - актор который хранит результаты тестов.
+   Обрабатывает следующие сообщения :
+   cообщение с результатом одного теста -> кладет его в локальное хранилище.
+   Сообщение с запросом результата теста → отвечает сообщением с  результатом всех тестов для заданного  packageId */
 }

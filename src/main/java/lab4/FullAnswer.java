d111fb194e0af039190a74f27b947ecfc55de46d
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 19:14:40 +0300

    Scripted auto-commit on change (2019-11-15 19:14:40) by gitwatch.sh

--- src/main/java/lab4/FullAnswer.java
+++ src/main/java/lab4/FullAnswer.java
@@ -1,4 +1,21 @@
 package lab4;
 
+import java.util.Map;
+
 public class FullAnswer {
+    private String packageId;
+    private Map<String, String> results;
+
+    public FullAnswer(String packageId, Map<String, String> results) {
+        this.packageId = packageId;
+        this.results = results;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public Map<String, String> getResults() {
+        return results;
+    }
 }

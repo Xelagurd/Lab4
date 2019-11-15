7dfed4a69f9578a342edf78201a20faa70f58057
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 18:55:21 +0300

    Scripted auto-commit on change (2019-11-15 18:55:20) by gitwatch.sh

--- src/main/java/lab4/JSTester.java
+++ src/main/java/lab4/JSTester.java
@@ -1,13 +1,20 @@
 package lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class JSTester extends AllDirectives {
 
@@ -23,6 +30,20 @@ public class JSTester extends AllDirectives {
         final ActorMaterializer materializer = ActorMaterializer.create(systemq);
 
         JSTester instance = new JSTester();
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute().flow(systemq, materializer);
+        final CompletionStage<ServerBinding> binding = httpser.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> systemq.terminate());
+
     }
 }
 

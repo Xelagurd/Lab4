48df12efd4730d0826c8ad80256c1838601e27dd
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 19:15:26 +0300

    Scripted auto-commit on change (2019-11-15 19:15:25) by gitwatch.sh

--- src/main/java/lab4/JSRunnerActor.java
+++ src/main/java/lab4/JSRunnerActor.java
@@ -7,4 +7,15 @@ public class JSRunnerActor extends AbstractActor {
     /*б. В приложении будем использовать следующие акторы :
       - актор который исполняет один тест из пакета.
       После исполнения теста результат передается актору хранилищу */
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(JSProgram.class, program -> {
+                    sender().tell(
+                            new StoreMessage(program.getPackageId(), program.getTest().getTestName(), program.run()), self());
+                    System.out.println("JSRunnerActor get task: " + program.getTest().getTestName() +
+                            "  with packageId: " + program.getPackageId() + "with result: " + program.run());
+                })
+                .build();
+    }
 }

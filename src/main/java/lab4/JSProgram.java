4f02f4afbb2180f06179c76158bf5352296576fa
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 19:17:05 +0300

    Scripted auto-commit on change (2019-11-15 19:17:04) by gitwatch.sh

--- src/main/java/lab4/JSProgram.java
+++ src/main/java/lab4/JSProgram.java
@@ -1,4 +1,28 @@
 package lab4;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.util.List;
+
 public class JSProgram {
-}
+    private String packageId;
+    private String jsScript;
+    private String functionName;
+
+    private List<Test> tests;
+
+    private Test test;
+
+    private static final ScriptEngine engine = new
+            ScriptEngineManager().getEngineByName("nashorn");
+
+    public JSProgram() {
+
+    }
+
+    public JSProgram(JSProgram prog) {
+        packageId = prog.getPackageId();
+        jsScript = prog.getJsScript();
+        functionName = prog.getFunctionName();
+        tests = prog.getTests();
+    }

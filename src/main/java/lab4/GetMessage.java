2264c83c19efc998e8e60131feb07b41047f77f9
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 19:00:07 +0300

    Scripted auto-commit on change (2019-11-15 19:00:07) by gitwatch.sh

--- src/main/java/lab4/GetMessage.java
+++ src/main/java/lab4/GetMessage.java
@@ -1,4 +1,13 @@
 package lab4;
 
 public class GetMessage {
+    private String packageId;
+
+    GetMessage(String packageId) {
+        this.packageId = packageId;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
 }

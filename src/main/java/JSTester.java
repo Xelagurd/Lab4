f4daf834383ee35b3f1d3586993410e02879f82d
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 18:47:18 +0300

    Scripted auto-commit on change (2019-11-15 18:47:17) by gitwatch.sh

--- src/main/java/JSTester.java
+++ src/main/java/JSTester.java
@@ -1,3 +1,18 @@
-public class JSTester {
+package lab4;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.server.AllDirectives;
+
+import java.io.IOException;
+
+public class JSTester extends AllDirectives {
+
+    static ActorRef master;
+
+    public static void main(String[] args) throws IOException {
+        /*а. Создаем actor system*/
+        ActorSystem systemq = ActorSystem.create("routes");
+    }
 }
+

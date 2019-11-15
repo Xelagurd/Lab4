733b95fcd89d4c99ee78655a42b104bfaa7d369b
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 19:04:59 +0300

    Scripted auto-commit on change (2019-11-15 19:04:58) by gitwatch.sh

--- src/main/java/lab4/StorageActor.java
+++ src/main/java/lab4/StorageActor.java
@@ -12,5 +12,15 @@ public class StorageActor extends AbstractActor {
    cообщение с результатом одного теста -> кладет его в локальное хранилище.
    Сообщение с запросом результата теста → отвечает сообщением с  результатом всех тестов для заданного  packageId */
 
-    
+    private Map<String, Map<String, String>> storage = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(StoreMessage.class, m -> {
+                })
+                .match(GetMessage.class, req -> sender().tell(
+                        new FullAnswer(req.getPackageId(), storage.get(req.getPackageId())), self())
+                ).build();
+    }
 }

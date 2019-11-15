548005256632269516276e3f284fe3ec7cc611a4
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 18:49:36 +0300

    Scripted auto-commit on change (2019-11-15 18:49:35) by gitwatch.sh

--- src/main/java/lab4/MasterActor.java
+++ src/main/java/lab4/MasterActor.java
@@ -1,4 +1,14 @@
 package lab4;
 
-public class MasterActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+import akka.routing.RoundRobinPool;
+
+public class MasterActor extends AbstractActor {
+    /*б. В приложении будем использовать следующие акторы :
+        - актор роутер
+        инициализирует актор хранилище а также пул акторов исполнителей тестов */
+    
 }

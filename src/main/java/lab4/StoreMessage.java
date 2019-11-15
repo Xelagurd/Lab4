712e8de5281a04c75b9a5e265275bc9ce9a83be2
DrAlexxD <alexander.drugakov@mail.ru>
2019-11-15 19:12:22 +0300

    Scripted auto-commit on change (2019-11-15 19:12:22) by gitwatch.sh

--- src/main/java/lab4/StoreMessage.java
+++ src/main/java/lab4/StoreMessage.java
@@ -1,4 +1,25 @@
 package lab4;
 
 public class StoreMessage {
+    private String packageId;
+    private String testName;
+    private String result;
+
+    public StoreMessage(String packageId, String testName, String result) {
+        this.packageId = packageId;
+        this.testName = testName;
+        this.result = result;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
